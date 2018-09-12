2915214a034991f1fb0945c473f9182861103c33
508821881@qq.com <508821881@qq.com>
2018-09-12 23:56:51 +0800

    [init]

---
+++ src/main/java/cn/yunlingfly/qiniuspringbootstarter/QiniuSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package cn.yunlingfly.qiniuspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiniuSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiniuSpringBootStarterApplication.class, args);
+	}
+}
