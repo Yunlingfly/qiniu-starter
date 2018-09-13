3ff4f89868f15e054e03b5e98a71aba1b828b207
508821881@qq.com <508821881@qq.com>
2018-09-13 11:03:34 +0800

    [ADD]qiniuSimpleUpLoad

---
+++ src/main/java/cn/yunlingfly/qiniuspringbootstarter/util/AccountMgr.java
@@ -0,0 +1,21 @@
+package cn.yunlingfly.qiniuspringbootstarter.util;
+
+/**
+ * 账户信息，获取授权认证
+ * @author yunligfly
+ *
+ */
+public class AccountMgr {
+    // AK
+    public static final String ACCESS_KEY = "U7vfxXTqFQ2D6qAzrzz9bguthIfd8dw2TZT2Hge9";
+    // SK
+    public static final String SECRET_KEY = "E2HvV26ShjnWihyfj8GOfJ2zQGByRAcJuIdVulMx";
+    // bucketName
+    public static final String BUCKETNAME="test";
+    // hyperlink-qiniu
+    public static final String HYPERLINK="http://ozcte1ybf.bkt.clouddn.com/";
+    // images-path
+    public static final String IMAGESPATH="/usr/SpringCloudServer/images/";
+    // images-path-local
+    public static final String IMAGESPATHLOCAL="images/";
+}
