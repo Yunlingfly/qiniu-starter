11e26a6a11fa3abdcc6424ef8580b168046375bb
508821881@qq.com <508821881@qq.com>
2018-09-16 13:08:37 +0800

    [FIX]描述

--- src/main/java/cn/yunlingfly/qiniuspringbootstarter/api/service/IQiniuService.java
+++ src/main/java/cn/yunlingfly/qiniuspringbootstarter/api/service/IQiniuService.java
@@ -14,11 +14,11 @@ public interface IQiniuService {
      * 上传文件
      * <p>文件上传</p>
      *
-     * @param file
-     * @param key
-     * @param existed
-     * @return
-     * @throws QiniuException
+     * @param file 填写上传文件File类型
+     * @param key 添加上传的key值
+     * @param existed 是否已经存在
+     * @return 返回com.qiniu.http.Response
+     * @throws QiniuException 抛出QiniuException异常
      */
     Response uploadFile(File file, String key, boolean existed) throws QiniuException;
 
@@ -26,20 +26,19 @@ public interface IQiniuService {
      * 上传文件
      * <p>文件路径上传</p>
      *
-     * @param filePath
-     * @param key
-     * @param existed
-     * @return
-     * @throws QiniuException
+     * @param filePath 填写上传文件的位置
+     * @param key 添加上传的key值
+     * @param existed 是否已经存在
+     * @return 返回com.qiniu.http.Response
+     * @throws QiniuException 抛出QiniuException异常
      */
     Response uploadFile(String filePath, String key, boolean existed) throws QiniuException;
 
     /**
      * 删除
      *
-     * @param key
-     * @return
-     * @throws QiniuException
+     * @param key 添加上传的key值
+     * @throws QiniuException 抛出QiniuException异常
      */
     void deleteFile(String key) throws QiniuException;
 }
